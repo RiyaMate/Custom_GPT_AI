218c83be527799360bfb5ecde3ccd4591da1bb25
RIYA SUBHASH MATE <145019243+RiyaMate@users.noreply.github.com>
2024-04-26 21:55:35 -0400

    First change

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { sourceCodePro } from "./styles/fonts";
 
 const Footer = () => {
@@ -8,13 +7,13 @@ const Footer = () => {
       className={`p-4 bg-gray-800 text-white w-full grid grid-cols-3 fixed bottom-0 ${sourceCodePro.className}`}
     >
       <p className={`text-center ${sourceCodePro.className}`}>
-        Taught by Shawn Esquivel
+        Riya Mate
       </p>
       <p className={`text-center ${sourceCodePro.className}`}>
-        &copy; Weeknights and Weekends {year}
+        &copy; AI AI {year}
       </p>
       <p className={`text-center ${sourceCodePro.className}`}>
-        Questions? Join the Discord
+        Questions? 
       </p>
     </footer>
   );
